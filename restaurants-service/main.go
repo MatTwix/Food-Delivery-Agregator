746eb97ccb7a319bab8cf44d391b18b253c3c01d
Matvey Fedorov <mfgolden@yandex.ru>
2025-07-30 16:57:04 +0300

    creating simple server

---
+++ restaurants-service/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Printf("Restaurants service is up and running!")
+	})
+
+	port := ":3001"
+	log.Printf("Starting restaurants service on port %s", port)
+
+	if err := http.ListenAndServe(port, nil); err != nil {
+		log.Fatalf("Failed to start service: %v", err)
+	}
+}
