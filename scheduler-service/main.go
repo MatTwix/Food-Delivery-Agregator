8ce9a5c2312fc1d54bf05da98e261e0c97d2c885
Matvey Fedorov <mfgolden@yandex.ru>
2025-08-24 19:38:10 +0300

    feat(scheduler): kafka producer + topics

--- scheduler-service/main.go
+++ scheduler-service/main.go
@@ -3,22 +3,37 @@ package main
 import (
 	"context"
 	"log/slog"
+	"os"
 	"os/signal"
 	"syscall"
 
 	"github.com/MatTwix/Food-Delivery-Agregator/scheduler-service/config"
+	"github.com/MatTwix/Food-Delivery-Agregator/scheduler-service/messaging"
 )
 
 func main() {
 	config.InitConfig()
 	config.InitLogger()
 
+	messaging.InitTopicsNames()
+
 	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
 	defer stop()
+
+	messaging.InitTopics()
+
+	kafkaProducer, err := messaging.NewProducer()
+	if err != nil {
+		slog.Error("failed to create Kafka producer", "error", err)
+		os.Exit(1)
+	}
 
 	<-ctx.Done()
 
 	slog.Info("shutting down servers")
+
+	kafkaProducer.Close()
+	slog.Info("Kafka producer closed")
 
 	slog.Info("service gracefully stopped")
 }
---
+++ scheduler-service/messaging/producer.go
@@ -0,0 +1,54 @@
+package messaging
+
+import (
+	"context"
+	"log/slog"
+	"os"
+	"strings"
+
+	"github.com/MatTwix/Food-Delivery-Agregator/scheduler-service/config"
+	"github.com/segmentio/kafka-go"
+)
+
+type Producer struct {
+	writer *kafka.Writer
+}
+
+func NewProducer() (*Producer, error) {
+	if config.Cfg.Kafka.Brokers == "" {
+		slog.Error("KAFKA_BROKERS environment variable is not set")
+		os.Exit(1)
+	}
+
+	brokers := strings.Split(config.Cfg.Kafka.Brokers, ",")
+
+	w := &kafka.Writer{
+		Addr:         kafka.TCP(brokers...),
+		Balancer:     &kafka.LeastBytes{},
+		RequiredAcks: kafka.RequireAll,
+	}
+
+	return &Producer{writer: w}, nil
+}
+
+func (p *Producer) Produce(ctx context.Context, topic string, key, value []byte) error {
+	err := p.writer.WriteMessages(ctx, kafka.Message{
+		Topic: topic,
+		Key:   key,
+		Value: value,
+	})
+	if err != nil {
+		slog.Error("failed to write message to Kafka topic", "topic", topic, "error", err)
+		return err
+	}
+
+	slog.Info("message sent to topic", "topic", topic)
+
+	return nil
+}
+
+func (p *Producer) Close() {
+	if err := p.writer.Close(); err != nil {
+		slog.Error("failed to close Kafka writer", "error", err)
+	}
+}
